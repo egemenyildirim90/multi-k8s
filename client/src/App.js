553a18bb5d791ded9e8daf80cd1f28001b79130a
egemenyildirim90 <egemenyildirim1@gmail.com>
2023-11-09 15:12:56 +0300

    new version

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
+import './App.css'; // Yeni stil dosyasını ekledik
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
-        <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">Fibonacci Calculator</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/otherpage">Other Page</Nav.Link>
+          </Nav>
+        </Navbar>
+
+        <div className="content-container"> {/* Yeni bir div ekledik */}
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
